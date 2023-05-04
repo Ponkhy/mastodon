a9a93e904502b17c8536e6b34817b8d107d077f1
Eugen Rochko <eugen@zeonfederated.com>
2023-05-04 12:00:33 +0200

    Change interaction modal in web UI

---
+++ app/controllers/api/v1/peers/search_controller.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Api::V1::Peers::SearchController < Api::BaseController
+  before_action -> { authorize_if_got_token! :read, :'read:search' }
+  before_action :set_domains
+
+  def index
+    render json: @domains
+  end
+
+  private
+
+  def set_domains
+    @domains = InstancesIndex.query(function_score: {
+      query: {
+        prefix: {
+          domain: params[:q],
+        },
+      },
+
+      field_value_factor: {
+        field: 'accounts_count',
+        modifier: 'log2p',
+      },
+    }).limit(10).pluck(:domain)
+  end
+end
